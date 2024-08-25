59fa7614efa10bb04f13f250a4de1e2e2f0ea9f8
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 22:03:41 +0300

    Add files via upload

---
+++ main (7).php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniveristesi
+
+*******************************************************************************/
+// IF YAPISI
+$sayi = 66;
+if ($sayi>0 and $sayi<50) {
+    echo(2);
+}
+else {
+    echo(4);
+}
+// 100 LÜK SİSTEMİ 5LİK SİSTEME ÇEVİRME KODU ANCAK ÇOK UGRASMAK ISTEMEDIM
+
+
+
+?>
